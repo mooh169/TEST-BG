db5438ee43c32f24978eee3212a291c69ae8cf8c
mooh169 <hamou6233@gmail.com>
2025-01-29 17:57:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["CX Dev", "🎮 Playing CX"];
+const statusMessages = ["CX Dev", "CX"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
